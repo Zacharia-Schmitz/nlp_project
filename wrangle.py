6d4e4ca5e930276aa05d4794e1330bf285a5c2e4
Zacharia Schmitz <schmitz.zacharia@gmail.com>
2023-10-18 10:36:24 -0500

    Name change

--- wrangle.py
+++ wrangle.py
@@ -588,3 +588,41 @@ def get_common_words(df, plot=True):
             plt.show()
 
     return common_words_df
+
+# Import necessary libraries
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+def generate_wordcloud(df, column='preprocessed_readme', word_count=50):
+
+    all_readmes = ' '.join(df['preprocessed_readme'])
+    all_words = re.findall(r'\w+', all_readmes.lower())
+    word_counts = Counter(all_words)
+    sorted_words = sorted(word_counts.items(), key=lambda x: x[1], reverse=True)
+
+    top_x = sorted_words[:word_count]
+    top_x_words = [word[0] for word in top_x]
+    top_x_counts = [count[1] for count in top_x]
+
+    # Create a dictionary of top 20 words and their counts
+    word_dict = dict(zip(top_x_words, top_x_counts))
+
+    # Generate wordcloud with blue color palette
+    wordcloud = WordCloud(width = 1500, height = 500, min_font_size = 10, max_font_size=50, colormap='Blues').generate_from_frequencies(word_dict)
+
+    # Plot the wordcloud
+    plt.figure(figsize = (21, 7), facecolor = None)
+    plt.imshow(wordcloud)
+    plt.axis("off")
+    plt.tight_layout(pad = 0)
+
+    # Show the plot
+    plt.show()
+
+
+
+
+
+
+
+
